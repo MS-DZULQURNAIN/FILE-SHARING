c803dac81f1d4a44e6b7060f2fbb74fdfa72a519
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-28 18:36:24 +0700

    Update DZULQURNAIN.py

--- MS/DZULQURNAIN.py
+++ MS/DZULQURNAIN.py
@@ -4,4 +4,6 @@ bcastp_text = "**Silahkan Forward Pesan PERTAMA dari Channel DataBase\n\nAtau Ki
 
 bcastt_text = "**Silahkan Forward Pesan TERAKHIR dari Channel DataBase\n\nAtau Kirim Link Postingan TERAKHIR dari Channel Database**"
 
+genlink_text = "**Silahkan Forward Pesan dari Channel DataBase\n\nAtau Kirim Link Postingan dari Channel Database**"
+
 souce_info_text = "**❖BOT FILE SHARING TELEGRAM❖\n\n✰ Owner Repo : @MSDZULQURNAIN\n⇝ Channel : @MSPR0JECT\n⇝ Support : @MsSUPP0RT\n\nᝣ 🄼🅂  𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍 ק𝙍♢JΞC† ᝣ**"
