58ad919295ac6fb653e0ccd1bdfff7589224f449
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-28 21:20:36 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"{source_info_text",
+            text = f"{source_info_text}",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
