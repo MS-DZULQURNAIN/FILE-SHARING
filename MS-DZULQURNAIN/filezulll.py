a2bf008659dede34a4a4c96332a40c428ad1ef53
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-28 21:31:07 +0700

    Create filezulll.py

---
+++ MS-DZULQURNAIN/filezulll.py
@@ -0,0 +1,9 @@
+#MSDZULQURNAIN
+
+bcastp_text = "**Silahkan Forward Pesan PERTAMA dari Channel DataBase\n\nAtau Kirim Link Postingan PERTAMA dari Channel Database**"
+
+bcastt_text = "**Silahkan Forward Pesan TERAKHIR dari Channel DataBase\n\nAtau Kirim Link Postingan TERAKHIR dari Channel Database**"
+
+genlink_text = "**Silahkan Forward Pesan dari Channel DataBase\n\nAtau Kirim Link Postingan dari Channel Database**"
+
+souce_info_text = "**❖BOT FILE SHARING TELEGRAM❖\n\n✰ Owner Repo : @MSDZULQURNAIN\n⇝ Channel : @MSPR0JECT\n⇝ Support : @MsSUPP0RT\n\nᝣ 🄼🅂  𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍 ק𝙍♢JΞC† ᝣ**"
