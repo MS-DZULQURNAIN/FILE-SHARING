e7299f908b10ac614580fafe92eaaa2a6bcdb31c
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-28 18:41:40 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,6 +5,7 @@ from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from MS.DZULQURNAIN import source_text
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
